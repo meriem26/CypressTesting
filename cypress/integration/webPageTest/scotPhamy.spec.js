a09e31e40cdda1a4f166c890c96f6e1aa318a6ab
Meriem <mmlys89@gmail.com>
2019-10-16 20:15:58 +0100

    Update files in repo
    Test for each field in the login page
    Update files in repo

--- cypress/integration/webPageTest/scotPhamy.spec.js
+++ cypress/integration/webPageTest/scotPhamy.spec.js
@@ -9,11 +9,27 @@ describe("Home page", function() {
   });
   it("Should contain a sign in button", () => {
     cy.contains("SIGN IN").click();
+    cy.get(".content").find();
   });
   it("Should contain as applicant button", () => {
     cy.contains("REGISTER AS APPLICANT").click();
   });
   it("Should contain Register as a company button", () => {
     cy.contains("REGISTER AS COMPANY").click();
+  });
+});
+describe("Sign in", function() {
+  it("Should be display login page", () => {
+    beforeEach(() => {
+      cy.visit("https://newscots-got-talent.herokuapp.com/login");
+    });
+  });
+
+  it("Contain an email field", () => {
+    cy.get(`[name ="email"]`).should("be.visible");
+  });
+
+  it("Contain a password field", () => {
+    cy.get(`[name ="password"]`).should("be.visible");
   });
 });
