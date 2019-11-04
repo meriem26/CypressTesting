39f22aadfdea3c6e988d4d8dc8bdd8e369b6c104
Meriem <mmlys89@gmail.com>
2019-11-04 11:52:56 +0000

    Add new tests for sign in

--- cypress/integration/phamySite/signIn.spec.js
+++ cypress/integration/phamySite/signIn.spec.js
@@ -9,4 +9,22 @@ describe("Sign in", function() {
   it("should contain a password field", () => {
     cy.get(`[name ="password"]`).should("be.visible");
   });
+  it("should take you to the home page if logged in successfully", () => {
+    cy.get(`[name ="email"]`).type("meriem@cyf.org");
+    cy.get(`[name ="password"]`).type("password");
+    cy.get(".form > button")
+      .contains("Login")
+      .click();
+    cy.contains("Logout").should("be.visible");
+    cy.contains("My Profile").should("be.visible");
+  });
+  it("should return an error message if entered the wrong details", () => {
+    cy.get(`[name ="email"]`).type("meriem@cyf.org");
+    cy.get(`[name ="password"]`).type("wrong password");
+    cy.get(".form > button")
+      .contains("Login")
+      .click();
+    cy.contains("Wrong Info. Try Again").should("be.visible");
+    cy.url().should("contain","/login");
+  });
 });
